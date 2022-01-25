8f159fc6369cbc6db9b4d962d7862f23ceeb7a09
bakjonghun <bakjonghun@hikick.kr>
2022-01-25 18:50:38 -0500

    add: estimate

--- src/pages/started/Estimate.js
+++ src/pages/started/Estimate.js
@@ -1,7 +1,13 @@
 import { useState } from 'react';
+import { NoStyledLink } from '../../components/NoStyledLink';
+import { StartedBottom } from '../../components/started/StartedBottom/StartedBottom';
+import { StartedBottomPrimary } from '../../components/started/StartedBottom/StartedBottomPrimary';
+import { StartedBottomSecondary } from '../../components/started/StartedBottom/StartedBottomSecondary';
 import { StartedDescription } from '../../components/started/StartedDescription';
+import { StartedEstimate } from '../../components/started/StartedEstimate/StartedEstimate';
+import { StartedEstimateItem } from '../../components/started/StartedEstimate/StartedEstimateItem';
 import { StartedHashtags } from '../../components/started/StartedHashtags';
-import { StartedLoading } from '../../components/started/StartedLoading';
+import { StartedIndicator } from '../../components/started/StartedIndicator';
 import { StartedTitle } from '../../components/started/StartedTitle';
 
 export const Estimate = () => {
@@ -12,24 +18,50 @@ export const Estimate = () => {
       <StartedTitle>견적서</StartedTitle>
       <StartedDescription>확인하고 바로 결제하러 갈까요?</StartedDescription>
       <StartedHashtags>#착한 #가격 #마이킥</StartedHashtags>
-      <StartedLoading loading={loading}>
+      {/* <StartedLoading loading={loading}>
         견적서를 확인하고 있습니다.
-      </StartedLoading>
-      {/* <StartedBottom>
-        <StartedIndicator current={1} />
+      </StartedLoading> */}
+      <StartedEstimate price={37000}>
+        <StartedEstimateItem description='킥보드 대여' price={37000} />
+        <StartedEstimateItem
+          description='헬멧 구매'
+          price={15000}
+          optional='1회'
+        />
+        <StartedEstimateItem description='마이세이프' price={37000} />
+        <StartedEstimateItem
+          description='마이케어'
+          price={15000}
+          optional='선결제'
+        />
+        <StartedEstimateItem
+          description='마이케어'
+          price={15000}
+          optional='선결제'
+        />
+        <StartedEstimateItem
+          description='마이케어'
+          price={15000}
+          optional='선결제'
+        />
+        <StartedEstimateItem
+          description='마이케어'
+          price={15000}
+          optional='선결제'
+        />
+      </StartedEstimate>
+      <StartedBottom>
+        <StartedIndicator current={3} />
         <NoStyledLink to='/started/signup'>
-          <Button block color='primary'>
-            <p style={{ fontSize: 23 }}>진행하기</p>
-            <p style={{ fontSize: 16 }}>월 16,000원 (첫 결제+)</p>
-          </Button>
+          <StartedBottomPrimary description='월 16,000원 (배송비 무료)'>
+            내 정보 입력하기
+          </StartedBottomPrimary>
         </NoStyledLink>
 
-        <NoStyledLink to='/started/signup'>
-          <AutoCenter style={{ marginTop: 20 }}>
-            <div style={{ fontSize: 16, width: '100%' }}>확인</div>
-          </AutoCenter>
+        <NoStyledLink to='/started'>
+          <StartedBottomSecondary>나중에 다시하기</StartedBottomSecondary>
         </NoStyledLink>
-      </StartedBottom> */}
+      </StartedBottom>
     </div>
   );
 };
