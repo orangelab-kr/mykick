7950c902e93a6f7c018db48aa6ab4f99ca94270a
bakjonghun <bakjonghun@hikick.kr>
2022-02-01 18:27:01 -0500

    add: useUser for easy management user state and allow to ignore dialog if client cannot request successfully

--- src/tools/client.js
+++ src/tools/client.js
@@ -6,10 +6,10 @@ export const Client = axios.create();
 export class ToastError extends Error {
   name = 'ToastError';
 
-  constructor(content) {
+  constructor(content, config) {
     super();
     this.message = content;
-    Toast.show({ icon: 'fail', content });
+    if (config?.alert) Toast.show({ icon: 'fail', content });
   }
 }
 
@@ -23,24 +23,26 @@ function getInterceptorRequest(config) {
   const accessKey = getAccessKey();
   config.baseURL = `${baseURL}`;
   config.headers = { authorization: accessKey };
+  config.alert = config?.alert === undefined ? true : config?.alert;
 
   return config;
 }
 
 function getInterceptorResponse(res) {
-  if (!res) throw new ToastError('서버와 연결할 수 없습니다.');
+  if (!res) throw new ToastError('서버와 연결할 수 없습니다.', res.config);
 
   const { data } = res;
-  if (data.opcode !== 0) throw new ToastError(data.message);
+  if (data.opcode !== 0) throw new ToastError(data.message, res.config);
   return res;
 }
 
 function getInterceptorResponseError(err) {
-  if (!err.response) throw new ToastError('서버와 연결할 수 없습니다.');
+  if (!err.response)
+    throw new ToastError('서버와 연결할 수 없습니다.', err.config);
 
   const { data } = err.response;
   if (data.opcode === 0) return err;
-  throw new ToastError(data.message);
+  throw new ToastError(data.message, err.config);
 }
 
 export function getAccessKey() {
---
+++ src/tools/useUser.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Client } from './client';
+
+let cachedUser;
+export const useUser = ({ clearCache }) => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    if (!clearCache && cachedUser) return setUser(cachedUser);
+    const updateUser = ({ data }) => {
+      cachedUser = data.user;
+      setUser(cachedUser);
+    };
+
+    Client.get('/auth', { alert: false })
+      .then(updateUser)
+      .catch(() => setUser(null));
+  }, [clearCache]);
+
+  return user;
+};
